0f71d822719441742c542eefa3cfb159de3e891a
Haoyuan <hzhong16@asu.edu>
2019-10-09 21:18:15 -0700

    complete assignment 2

--- CSE360assign2/src/cse360assign2/AddingMachine.java
+++ CSE360assign2/src/cse360assign2/AddingMachine.java
@@ -1,30 +1,70 @@
+/*
+ * Author : Haoyuan Zhong
+ * ASU ID : 1213565598
+ * class ID: 70642 Wednesday
+ * assignment : assignment 2
+ * 
+ * DESCRIPTION : This is a simple program for simple calculation
+ *
+ *URL : https://github.com/hzhong16/AddingMachine/blob/master/CSE360assign2/src/cse360assign2/AddingMachine.java 
+ * 
+ */
 package cse360assign2;
 
+/*
+ * class for adding function
+ */
 public class AddingMachine {
 	
-private int total;
+	private int total;
+	private String history;
 	
+	/*
+	 * for initial value of total and history
+	 */
 	public AddingMachine () {
-		total = 0;  // not needed - included for clarity
+		total = 0; // not needed - included for clarity
+		history = " 0 ";
 	}
 	
+	/*
+	 * return the answer
+	 */
 	public int getTotal () {
-		return 0;
+		return total;
 	}
 	
+	/*
+	 * adding function
+	 */
 	public void add (int value) {
-		
+		total += value;
+		history += " + " + value;
 	}
-	
+	/*
+	 * subtract function
+	 */
 	public void subtract (int value) {
-		
+		total -= value;
+		history += " - " + value;
 	}
-		
+	/*  
+	 *return history to the function
+	 */
 	public String toString () {
-		return "";
+		return history;
 	}
-
-	public void clear() {
 	
-	}
+	/*  
+	 * set everything back to initial value start over
+	 */
+	public void clear() {
+		history = "0";
+		total = 0;
+	};
+	
+	
+
+
+
 }
---
+++ CSE360assign2/src/cse360assign2/myCalculator.java
@@ -0,0 +1,23 @@
+package cse360assign2;
+
+public class myCalculator {
+	public static void main(String[] args)
+	{
+		AddingMachine myCalculator = new AddingMachine();
+		
+		myCalculator.add(4);
+		myCalculator.subtract(2);
+		myCalculator.add(5);
+		
+		System.out.println(myCalculator.toString());
+		System.out.println("total: " + myCalculator.getTotal());
+		
+		myCalculator.clear();
+		
+		myCalculator.add(6);
+		myCalculator.add(6);
+		System.out.println(myCalculator.toString());
+		
+		
+	}
+}
